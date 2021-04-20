8fa718f58b8048c281fcc3c80d902428f6dee95d
Pedro Albuquerque <pedrowevernek@gmail.com>
2021-04-19 22:44:38 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          wevernek
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/pedro-albuquerque-157156138/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
         </a>
       </header>
     </div>
